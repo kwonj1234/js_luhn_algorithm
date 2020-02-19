2506968db897d96b4ebbc486e8d484fcc804a4a8
kwonj1234 <kwonj1234@gmail.com>
2020-02-18 20:23:34 -0500

    almost

--- luhn.js
+++ luhn.js
@@ -1,28 +1,59 @@
 function luhn() {
-    let cardNum = document.getElementById('cardNumber')
+    cardNum = document.getElementById('cardNumber')
     
-    cardNum.toString()
     let arr = []
-    for (let i = 0; i < cardNum.length; i++) {
+
+    for (let i = 0; i < cardNum.value.length; i++) {
         if (i % 2 === 0) {
-            number = cardNum[i].toNumber() * 2
+            number = cardNum.value.charAt(i) * 2
             if (number > 9) {
+                console.log(number)
                 number = number.toString().split()
+                arr.push(1)
+                arr.push(number % 10)
+            } else {
+                arr.push(Number(number))
             }
-            console.log(number)
-            arr.push(number)
+        } else {
+            arr.push(Number(cardNum.value.charAt(i)))
         }
     }
-
+    console.log(arr)
     sum = 0
     
     for (let i = 0; i < arr.length; i++) {
-        sum += arr[i].toNumber()
+        sum += arr[i]
     }
-    if (sum % 10) {
-        text = "Valid"
+    console.log(sum)
+    if (sum % 10 === 0) {
+        if (cardNum.value.length === 16){
+            if (cardNum.value.charAt(0) === 4) {
+                text = "Valid Visa"
+            } else if (cardNum.value.charAt(0) === 5 &&
+                        cardNum.value.charAt(1) === 1 ||
+                        cardNum.value.charAt(1) === 2 ||
+                        cardNum.value.charAt(1) === 3 ||
+                        cardNum.value.charAt(1) === 4 ||
+                        cardNum.value.charAt(1) === 5) {
+                text = "Valid Mastercard"
+            } else if (cardNum.value.charAt(0) === 6 &&
+                        cardNum.value.charAt(1) === 0 &&
+                        cardNum.value.charAt(2) === 1 &&
+                        cardNum.value.charAt(3) === 1) {
+                text = "valid Discover"
+            }
+
+        } else if (cardNum.value.length === 15) {
+            if (cardNum.value.charAt(0) === 3 &&
+                cardNum.value.charAt(1) === 4 ||
+                cardNum.value.charAt(1) === 7) {
+                text = "Valid AMEX" 
+                }
+        } else {
+            text = "Invalid Credit Card Number"
+        }
     } else {
-        text = "Invalid"
+        text = "Invalid Credit Card Number"
     }
 
     document.getElementById("output").innerHTML = "<p>" + text + "</p>"
