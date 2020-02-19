db8cf38e550fb826f57be86663d8951714d42bc3
kwonj1234 <kwonj1234@gmail.com>
2020-02-19 10:34:38 -0500

    MVP

--- luhn.js
+++ luhn.js
@@ -27,34 +27,43 @@ function luhn() {
     console.log(sum)
     if (sum % 10 === 0) {
         if (cardNum.value.length === 16){
-            if (cardNum.value.charAt(0) === 4) {
+            if (cardNum.value.charAt(0) === "4") {
                 text = "Valid Visa"
-            } else if (cardNum.value.charAt(0) === 5 &&
-                        cardNum.value.charAt(1) === 1 ||
-                        cardNum.value.charAt(1) === 2 ||
-                        cardNum.value.charAt(1) === 3 ||
-                        cardNum.value.charAt(1) === 4 ||
-                        cardNum.value.charAt(1) === 5) {
+            } else if (cardNum.value.charAt(0) === "5" &&
+                        (cardNum.value.charAt(1) === "1" ||
+                        cardNum.value.charAt(1) === "2" ||
+                        cardNum.value.charAt(1) === "3" ||
+                        cardNum.value.charAt(1) === "4" ||
+                        cardNum.value.charAt(1) === "5")) {
                 text = "Valid Mastercard"
-            } else if (cardNum.value.charAt(0) === 6 &&
-                        cardNum.value.charAt(1) === 0 &&
-                        cardNum.value.charAt(2) === 1 &&
-                        cardNum.value.charAt(3) === 1) {
-                text = "valid Discover"
+            } else if (cardNum.value.charAt(0) === "6" &&
+                        cardNum.value.charAt(1) === "0" &&
+                        cardNum.value.charAt(2) === "1" &&
+                        cardNum.value.charAt(3) === "1") {
+                text = "Valid Discover"
+            } else {
+                text = "Invalid Credit Card Number"
             }
-
+            
         } else if (cardNum.value.length === 15) {
-            if (cardNum.value.charAt(0) === 3 &&
-                cardNum.value.charAt(1) === 4 ||
-                cardNum.value.charAt(1) === 7) {
+            console.log(cardNum.value.charAt(1))
+            if ((cardNum.value.charAt(1) === "4" ||
+                cardNum.value.charAt(1) === "7") &&
+                cardNum.value.charAt(0) === "3") {
                 text = "Valid AMEX" 
-                }
+                console.log(text)
+            } else {
+                text = "Invalid Credit Card Number"
+            }
+            
         } else {
             text = "Invalid Credit Card Number"
         }
+
     } else {
         text = "Invalid Credit Card Number"
     }
-
     document.getElementById("output").innerHTML = "<p>" + text + "</p>"
 }
+
+// 347650202246894
